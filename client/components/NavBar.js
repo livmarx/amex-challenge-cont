0dfea71e6080ba8622a2e9cfb4411a288624fe52
Liv Marx <oehrenmarks@gmail.com>
2019-01-12 17:28:30 -0500

    feat(navbar):add  toggle button

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,34 +2,59 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    let mainNav = document.getElementById('js-menu');
+    let navBarToggle = document.getElementById('js-navbar-toggle');
+
+    navBarToggle.addEventListener('click', function() {
+      mainNav.classList.toggle('active');
+    });
+  }
+
   render() {
     return (
-      <nav className="nav">
-        <div className="logo">
-          <Link to="/" className="single-link">
-            <img
+      <nav className="navbar">
+        <span
+          className="navbar-toggle"
+          id="js-navbar-toggle"
+          onClick={this.toggleMenu}
+        >
+          <i className="fas fa-bars" />
+        </span>
+        <Link to="/" className="logo">
+          {/* <img
               src="https://unixtitan.net/images/book-clipart-5.png"
               className="bookself-img"
-            />
-          </Link>
-        </div>
-        <div className="nav-links">
-          <Link to="/" className="single-link">
-            <button type="button" className="myButton">
+            /> */}
+          logo
+        </Link>
+        <ul className="main-nav" id="js-menu">
+          <li>
+            <Link to="/" className="nav-links">
+              {/* <button type="button" className="myButton"> */}
               Home
-            </button>
-          </Link>
-          <Link to="/general-search" className="single-link">
-            <button type="button" className="myButton">
+              {/* </button> */}
+            </Link>
+          </li>
+          <li>
+            <Link to="/general-search" className="nav-links">
+              {/* <button type="button" className="myButton"> */}
               General Search
-            </button>
-          </Link>
-          <Link to="/advanced-search" className="single-link">
-            <button type="button" className="myButton">
+              {/* </button> */}
+            </Link>
+          </li>
+          <li>
+            <Link to="/advanced-search" className="nav-links">
+              {/* <button type="button" className="myButton"> */}
               Advanced Search
-            </button>
-          </Link>
-        </div>
+              {/* </button> */}
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
