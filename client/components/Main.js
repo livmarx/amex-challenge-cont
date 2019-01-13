788844f3bc66ad90bc8d778e0f3d5d08cbbf830e
Liv Marx <oehrenmarks@gmail.com>
2019-01-13 13:13:24 -0500

    feat(styling): add shadows

--- client/components/Main.js
+++ client/components/Main.js
@@ -10,6 +10,7 @@ class Main extends React.Component {
         <NavBar />
         <div className="title">
           <h1>bÜkshelph</h1>
+          <br />
         </div>
         <br />
         <div id="about">
@@ -21,22 +22,19 @@ class Main extends React.Component {
             fire-side-dwellers, hot-tea-sippers, and stay-too-long-in-beders.
             Because at bÜkshelph, we believe that everyone should have the
             freedom to stay inside.
-          </p>
-          <h6>----------------------------------------</h6>
-          <p>
+            <br />
+            <br />
             Use our General Search to find books by your favorite author or all
             the books from your favorite series. For you serious book-worms, we
             also provide an Advanced Search option for you to find the specific
             edition you can't live without. We support searches by ISBN, OCLC,
             OLID, and LCCN.
-          </p>
-          <p>Happy reading to you and yours!</p>
-          <h6>----------------------------------------</h6>
-          <p>
-            If you don't know where to start your reading journy, check out the
-            articles below!
+            <br />
+            <br />
+            Happy reading to you and yours!
           </p>
         </div>
+        <br />
         <Articles />
         <Footer />
       </div>
